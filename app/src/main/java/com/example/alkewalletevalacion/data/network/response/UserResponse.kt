277263b064ff482dbdfdd284d47b6f17bff204a7
Felipe Toro Galdámez - galtor <felipetorog@gmail.com>
2024-07-01 20:22:04 -0400

    4.6

--- app/src/main/java/com/example/alkewalletevalacion/data/network/response/UserResponse.kt
+++ app/src/main/java/com/example/alkewalletevalacion/data/network/response/UserResponse.kt
@@ -3,6 +3,7 @@ package com.example.alkewalletevalacion.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class UserResponse(
+    @SerializedName("id") val id:Int?,
     @SerializedName("first_name") val firstName: String?,
     @SerializedName("last_name") val lastName: String?,
     @SerializedName("email") val email: String?,
